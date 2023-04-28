36e6244d34f4c5a9daa34145140e9f72147b69b5
Egor Kruglov <yegorkruglov@gmail.com>
2023-04-28 17:02:58 +0300

    added outlets, uicolor extension, changed colours and corner radius

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -7,13 +7,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    @IBOutlet var colorView: UIView!
+    @IBOutlet var colorizeButton: UIButton!
+    @IBOutlet var redValue: UILabel!
+    @IBOutlet var greenValue: UILabel!
+    @IBOutlet var blueValue: UILabel!
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        colorView.layer.cornerRadius = 20
+        colorizeButton.layer.cornerRadius = 10
+
+        colorView.backgroundColor = UIColor.random
     }
+    
+    @IBAction func colorizeButtonTapped() {
+    }
+    
+    @IBAction func redSliderAction() {
+    }
+    
+    @IBAction func greenSliderAction() {
+    }
+    
+    @IBAction func blueSliderAction() {
+    }
+    
+}
 
-
+extension UIColor {
+    
+    class var random: UIColor {
+        return UIColor(red: .random(in: 0.3...1),
+                       green: .random(in: 0.3...1),
+                       blue: .random(in: 0.3...1),
+                       alpha: 1.0)
+    }
 }
 
